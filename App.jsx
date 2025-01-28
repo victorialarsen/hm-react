4db65579d61e285546fc799c5ad2b6405d96c438
“Victoria <victoriialarsen@gmail.com>
2025-01-28 20:58:25 +0200

    files added

--- App.jsx
+++ App.jsx
@@ -1,17 +1,15 @@
-<h3> Proj React Homework </h3>
-
 import React from 'react';
 import Card from './Card';
 
 const App = () => {
   const data = [
     {
-      img: 'https://images.pexels.com/photos/4587994/pexels-photo-4587994.jpeg',
+      img: 'image.jpg',
       title: 'Cute Dog 1',
       description: 'A cute dog enjoying the sunshine.',
     },
     {
-      img: 'https://images.pexels.com/photos/4588045/pexels-photo-4588045.jpeg',
+      img: 'image1.jpg',
       title: 'Cute Dog 2',
       description: 'Another adorable dog playing in the park.',
     },
